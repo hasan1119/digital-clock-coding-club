eaa729e9a19c54455e3115b47a8c50340e8f8aa2
Md. Rokibul Hasan <hasan.webappsdeveloper@gmail.com>
2021-09-04 22:44:57 +0600

    digital clock

---
+++ app.js
@@ -0,0 +1,41 @@
+var hourText = document.getElementById('hour')
+var minuteText = document.getElementById('minute')
+var secondText = document.getElementById('second')
+
+
+var hour = 0
+var minute = 0
+var second = 0
+setInterval(function () {
+    second++
+
+
+    if (second == 60) {
+        if (minute == 60) {
+            minute = 0
+            hour++;
+            if ((hour + '').length == 1) {
+
+                hourText.innerText = "0" + hour;
+            } else {
+                hourText.innerText = hour;
+            }
+        }
+        if ((minute + '').length == 1) {
+            minuteText.innerText = '0' + minute;
+        } else {
+            minuteText.innerText = minute;
+        }
+
+        minute++
+        second = 0
+    }
+
+    if ((second + '').length == 1) {
+        secondText.innerHTML = '0' + second;
+    } else {
+        secondText.innerHTML = second;
+    }
+
+
+}, 1)
